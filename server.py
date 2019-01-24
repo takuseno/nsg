644afee52edb8a93001aae13fab32bba2a6d92f3
takuseno <takuma.seno@gmail.com>
2019-01-24 22:19:20 +0900

    Add error logging

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import os
 import base64
+import logging
+import traceback
 
 from io import BytesIO
 from utils import get_original_link
@@ -8,6 +10,11 @@ from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__)
+
+
+logging.basicConfig(level=logging.DEBUG)
+LOGGER = logging.getLogger(__name__)
+
 
 @app.route('/')
 def index():
@@ -23,6 +30,7 @@ def search():
         image.save(buf, format='png')
         image_str = base64.b64encode(buf.getvalue()).decode('utf-8')
     except Exception as e:
+        LOGGER.error(traceback.format_exc())
         image_str = None
         result = str(e)
     return render_template('index.html', result=result, image=image_str)
