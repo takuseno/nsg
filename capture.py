e0bc60a4e3fc8a05760806e02c992d5f2d18428d
takuseno <takuma.seno@gmail.com>
2019-01-24 22:12:40 +0900

    Implement screenshot

---
+++ capture.py
@@ -0,0 +1,13 @@
+from PIL import Image
+from selenium import webdriver
+
+
+def capture(url):
+    driver = webdriver.PhantomJS()
+    driver.get(url)
+    driver.set_window_size(1024, 768)
+    driver.save_screenshot('static/screenshot.png')
+    image = Image.open('static/screenshot.png')
+    image = image.crop((0, 0, 1024, 720))
+    image.save('static/screenshot.png')
+    return image
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 import os
+import base64
 
+from io import BytesIO
 from utils import get_original_link
+from capture import capture
 from flask import Flask, render_template, request, redirect
 
 
@@ -8,16 +11,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', result='')
+    return render_template('index.html', result='', image=None)
 
 @app.route('/search', methods=['POST'])
 def search():
     url = request.form['url']
     try:
-        message = get_original_link(url)
+        result = get_original_link(url)
+        image = capture(result)
+        buf = BytesIO()
+        image.save(buf, format='png')
+        image_str = base64.b64encode(buf.getvalue()).decode('utf-8')
     except Exception as e:
-        message = e.message
-    return render_template('index.html', result=message)
+        image_str = None
+        result = str(e)
+    return render_template('index.html', result=result, image=image_str)
 
 if __name__ == '__main__':
     app.debug = True
