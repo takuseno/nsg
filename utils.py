ab85e538ac733e3529b272622b0185ff003ea09f
takuseno <takuma.seno@gmail.com>
2019-01-26 14:15:38 +0900

    Implement vitual google search

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import logging
 
 from urllib.parse import urlencode
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -29,29 +30,31 @@ def get_title_and_url(url):
 
 def get_top_google_search_link(title, site):
     query = title + ' site:' + site
-    google_url = 'https://www.google.co.jp/search'
-    url = google_url + '?' + urlencode({'q': query})
     # use headless browser to avoid bot detection
     driver = webdriver.PhantomJS()
-    driver.get(url)
+    # google search
+    driver.get('https://www.google.co.jp?pws=0')
+    driver.find_element_by_name('q').send_keys(query)
+    driver.find_element_by_name('q').send_keys(Keys.RETURN)
+    # check bot detection
     LOGGER.debug(driver.current_url)
     if driver.current_url.find('www.google.com/sorry/index') > -1:
         raise Exception('we are detected as a bot by Google now. Go to <a href="{}">here</a>.'.format(url))
+    # get the first result
     links = driver.find_elements_by_css_selector('h3 > a')
     url = links[0].get_attribute('href')
     if url.find('/search?q=') > -1:
         url = links[1].get_attribute('href')
-    return url.replace('https://www.google.co.jp', '').replace('/url?q=', '')
+    return clean_url(url)
 
 def clean_url(url):
     end_index = url.find('&')
     if end_index > -1:
-        return url[:end_index]
-    return url
+        url = url[:end_index]
+    return url.replace('https://www.google.co.jp', '').replace('/url?q=', '')
 
 def get_original_link(url):
     if url.find('news.yahoo.co.jp/pickup') > -1:
         url = get_article_link(url)
     title, site = get_title_and_url(url)
-    original_url = get_top_google_search_link(title, site)
-    return clean_url(original_url)
+    return get_top_google_search_link(title, site)
